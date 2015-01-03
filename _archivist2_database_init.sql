cc5c2dca81d9c53a0ff8708eb4d8be890b097060
pbiecek <przemyslaw.biecek@gmail.com>
2015-01-03 23:16:37 +0100

    sql query for mysql

--- _archivist2_database_init.sql
+++ _archivist2_database_init.sql
@@ -1,4 +1,15 @@
 create database archivist2;
 use archivist2;
 
+create table IF NOT EXISTS artifact (
+md5hash VARCHAR(35),
+name VARCHAR(512),
+createdDate VARCHAR(25)
+);
+
+create table IF NOT EXISTS tag (
+md5hash VARCHAR(35),
+tag TEXT,
+createdDate VARCHAR(25)
+);
 
